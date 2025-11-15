63adf14b0ae247b1ccad24e30ceb1788848d9526
batraccc <batracmatija4@gmail.com>
2025-11-15 21:40:50 +0100

    Add functionality to search books

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import dotenv from "dotenv";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,8 @@ dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+let search_results = [];
 
 const db = new pg.Client({
     user: process.env.DB_USER,
@@ -22,6 +25,20 @@ db.connect();
 
 app.get("/", async (req, res) => {
     res.render("index.ejs");
+})
+
+app.get("/search", async (req, res) => {
+    res.render("search.ejs");
+})
+
+app.post("/search", async (req, res) => {
+    const book_name = (req.body.book_name).trim().replace(/\s+/g, '+').toLowerCase();
+    try{
+        const result = await axios.get(`https://openlibrary.org/search.json?q=${book_name}`);
+        res.render("search.ejs", {search_results: result.data.docs});
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 app.listen(port, () => {
